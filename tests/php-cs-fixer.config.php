6bc2c062f1a69578e46a7743de5ca5b92e447934
Kuba Werłos <werlos@gmail.com>
2016-12-07 08:22:39 +0100

    Configuration improvements

--- tests/php-cs-fixer.config.php
+++ tests/php-cs-fixer.config.php
@@ -6,7 +6,6 @@ return PhpCsFixer\Config::create()
     ->setRules(array(
         '@Symfony' => true,
         '@Symfony:risky' => true,
-//        'declare_strict_types' => true,
         'dir_constant' => true,
         'ereg_to_preg' => true,
         'mb_str_functions' => true,
@@ -14,7 +13,7 @@ return PhpCsFixer\Config::create()
         'no_php4_constructor' => true,
         'php_unit_strict' => true,
         'psr4' => true,
-//        'simplified_null_return' => true,
+        'simplified_null_return' => true,
         'strict_comparison' => true,
         'strict_param' => true,
     ))
